bd666ae7f1fef365c41dd11281d7151c4bfa45bf
QAQguowang <982639556@qq.com>
2021-04-24 21:44:40 +0800

    Django_study01

--- djangoProject01/djangoProject01/urls.py
+++ djangoProject01/djangoProject01/urls.py
@@ -1,4 +1,4 @@
-"""chapter_01 URL Configuration
+"""djangoProject01 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import testdb
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('testdb/', testdb.testdb),
 ]
--- djangoProject01/manage.py
+++ djangoProject01/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chapter_01.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoProject01.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
