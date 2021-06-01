74a0d09b5377f231f85a75f2b88dd2ab56687a46
QAQguowang <982639556@qq.com>
2021-06-01 22:41:05 +0800

    Django企业开发实战-day07

--- typeidea/config/views.py
+++ typeidea/config/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 
 
 # Create your views here.
+from django.views.generic import ListView
 
-def links(resquest):
-    return HttpResponse('links')
+from blog.views import CommonViewMixin
+from config.models import Link
+
+
+class LinkListView(CommonViewMixin, ListView):
+    queryset = Link.objects.filter(status=Link.STATUS_NORMAL)
+    template_name = 'config/links.html'
+    context_object_name = 'link_list'
--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -17,8 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import PostDetailView, PostListView
-from config.views import links
+from blog.views import PostDetailView, PostListView, SearchView, AuthorView
+from config.views import LinkListView
 from typeidea.custom_site import custom_site
 
 from blog.views import (
@@ -31,7 +31,9 @@ urlpatterns = [
     url(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
     url(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
     url(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
-    url(r'^links/$', links, name='links'),
     url(r'^super_admin/', admin.site.urls, name='super-admin'),
     url(r'^admin/', custom_site.urls, name='admin'),
+    url(r'^search/$', SearchView.as_view(), name='search'),
+    url(r'^author/(?P<owner_id>\d+)/$', AuthorView.as_view(), name='author'),
+    url(r'^links/$', LinkListView.as_view(), name='links'),
 ]
